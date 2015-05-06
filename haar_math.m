1e4562c0ef786f2777574fdbca66ff3dd6da1ec8
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2015-05-05 20:49:40 -0400

    added haar math code

--- haar_math.m
+++ haar_math.m
@@ -1,13 +1,14 @@
 clear all; close all;
-orig = imread('hepburn.jpg'); %real pic format
-%orig = rgb2gray(orig2);
-x = im2double(orig);
+orig = imread('hepburn.jpg'); % grayscale format
+x = im2double(orig); % double-precision floating-point number
 
-[m, n] =size(x)
+% First, we need to find the different number of divisions made to the image to get 8 by 8 blocks that will have the transform implemented on:
+    
+[m, n] = size(x)
 divisions_row = m/8;
 divisions_column = n/8;
 
-new = zeros(m,n);
+new = zeros(m,n); % new matrix of the transformed image
 
 haar = [1.0/8.0 1.0/8.0 1.0/4.0 0 1.0/2.0 0 0 0;
     1.0/8.0 1.0/8.0 1.0/4.0 0 -1.0/2.0 0 0 0;
@@ -16,16 +17,19 @@ haar = [1.0/8.0 1.0/8.0 1.0/4.0 0 1.0/2.0 0 0 0;
     1.0/8.0 -1.0/8.0 0 1.0/4.0 0 0 1.0/2.0 0;
     1.0/8.0 -1.0/8.0 0 1.0/4.0 0 0 -1.0/2.0 0;
     1.0/8.0 -1.0/8.0 0 -1.0/4.0 0 0 0 1.0/2.0;
-    1.0/8.0 -1.0/8.0 0 -1.0/4.0 0 0 0 -1.0/2.0];
-
+    1.0/8.0 -1.0/8.0 0 -1.0/4.0 0 0 0 -1.0/2.0]; % Linear algebra approach, which is a matrix multiplication version of the Haar wavelet transform. This matrix is the equivalent of finding the average and difference of every pair in every row and every column
+    
+% The following code, which transforms every division created from the image matrix
 for j = 1:divisions_row
     for k = 1:divisions_column
         new(1+8*(j-1):8*(j),1+8*(k-1):8*(k)) = transpose(haar)*x(1+8*(j-1):8*(j),1+8*(k-1):8*(k))*haar;
     end
 end
 
-final = uint8(new*255);
+        
+final = uint8(new*255); % make a version of the final image that has grayscale format
 
+% The following code finds the level of attenuation in the original image:
 count_orig = 0;
 count_not = 0;
 for i = 1:length(orig(:,1))
@@ -37,7 +41,22 @@ for i = 1:length(orig(:,1))
         end
     end
 end
+            
+% The following code finds the level of attenuation in the haar transformed image:
+count_transformed = 0;
+count_notzero = 0;
+for i = 1:length(final(:,1))
+    for j = 1:length(final(1,:))
+        if final(j,i) == 0
+            count_transformed = count_transformed + 1;
+        else
+            count_notzero = count_notzero +1;
+        end
+    end
+end
+            
 total_coeff = m*n;
-count_orig*100/total_coeff
+original_attenuation = count_orig*100/total_coeff
+transformed_attenuation = count_transformed*100/total_coeff
 figure; imshow(x)
 figure; imshow(new)
